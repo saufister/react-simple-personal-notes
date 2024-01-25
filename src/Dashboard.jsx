74bd097dbcb440816519717ea263fba84f3a8900
PDAMBJM\saufi <saufi@pdambandarmasih.com>
2024-01-25 14:55:45 +0800

    initial commit

---
+++ src/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//disini nanti return 3 buah komponen 
+//1. Header
+//2. Catatan Aktif 
+//3. Arsip 
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
